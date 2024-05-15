eb4076ec6bea304dc504205fa44e65af084357e4
ferrovovan <ferrovovan@gmail.com>
2024-05-15 13:18:57 +0700

    final

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-1. Учебный проект на Logisin и CDM-16  "Змейка".
-2. Процедурная генерация элементов схемы.
-3. [Процессоры от Иртегова](https://github.com/dmitry-irtegov/cdm-devkit)
-4. [Образец](https://github.com/JooudDoo/Logisim-Snake)
+# Учебный проект на Logisin и CDM-8 mark 5   "Змейка".
+![LOGO](https://github.com/ferrovovan/cdm_project/blob/main/Preview.png)
+1. Процедурная генерация элементов схемы.
+2. [Процессоры от Иртегова](https://github.com/dmitry-irtegov/cdm-devkit)
+3. [Образец](https://github.com/JooudDoo/Logisim-Snake)
+4. Создать ssh-ключ:
+  ssh-keygen -t rsa -b 4096 -C "комментарий"
--- source/SnakeCode.asm
+++ source/SnakeCode.asm
@@ -1,24 +1,21 @@
-			asect 0xF0 # A memory cell used to send instructions
-	readyFlags: 	   # to the processor from external circuitry
-	
-			asect 0xF1 # A memory cell used to exchange segments 
-	parts:			   # between the processor and external circuitry
-	
-			asect 0xF2 # Pointer to the beginning of the queue
+			asect 0xF0 
+	readyFlags: 	   
+			asect 0xF1 
+	parts:			  
+			asect 0xF2 
 	begQueue:
-			asect 0xF3 # Pointer to the end of the queue
+			asect 0xF3 
 	endQueue:
-			asect 0xF4 # Memory cell storing the queue size
+			asect 0xF4 
 	sizeQueue:		
 			asect 0x00
 	main:				
 			ldi r0, readyFlags
 			ld r0, r1
 			
-			if # Game status check{
+			if 
 				tst r1
 			is pl
-				# Initialization of variables{ 
 				ldi r0, 0x00
 				ldi r1, begQueue
 				st r1, r0				
@@ -33,32 +30,29 @@
 				
 				ldi r1, sizeQueue
 				ldi r0, 1
-				st r1, r0 #}
+				st r1, r0
 				
-				# Waiting for the game to start
 				ldi r0, readyFlags
 				do	
 					ld r0, r1
 					tst r1
 				until mi
 			fi
-			#}
 			
 			
 			
-			if # Checking for a request to save a new segment to the queue
+			if 
 				shl r1
 				tst r1
 			is mi
 				ldi r2, parts
 				ld r2, r2
 				move r2, r3
-				if # If the segment length is zero, we do not save it
+				if 
 					shl r3
 					shl r3
 					tst r3
 				is nz
-					# Saving a new segment and incrementing the queue size {
 					ldi r0, endQueue
 					ld r0, r0
 					st r0, r2
@@ -70,13 +64,11 @@
 					inc r2
 					ld r2, r0
 					inc r0
-					st r2, r0 #}
+					st r2, r0
 					
-					# Memory end check and pointer move to beginning (0x00){
 					ldi r0, endQueue
 					ld r0, r0
 					ldi r2, 0xF0
-					
 					if
 						cmp r0, r2
 					is eq
@@ -84,17 +76,16 @@
 						ldi r0, endQueue
 						st r0, r2
 					fi
-					#}
 				fi
 			fi
 			
 			
 			
-			if # Checking for the request to issue the next segment in line to the tail
+			if 
 				shl r1
 				tst r1
 			is mi
-				if # If the queue is not empty, unload the segment and decrement the queue size 
+				if 
 					ldi r0, sizeQueue
 					ld r0, r0
 					tst r0
@@ -115,7 +106,6 @@
 					ldi r2, begQueue
 					st r2, r0
 					
-					# Memory end check and pointer move to beginning (0x00){
 					ldi r0, begQueue
 					ld r0, r0
 					ldi r2, 0xF0
@@ -126,7 +116,6 @@
 						ldi r0, begQueue
 						st r0, r2
 					fi
-					#}
 				fi
 			fi				
 	br main
